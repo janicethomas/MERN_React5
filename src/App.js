f90cffd9e39c5d210a6945949c6e9efdfabbbfca
janicethomas <janice.thomas@outlook.com>
2023-10-12 00:42:11 +0530

    uplaod

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import DatePicker from "react-date-picker";
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
+  const currDate = new Date();
+  const [dateValue, onDateChange] = useState(new Date());
+  const [diffValue, onDiffChange] = useState(0);
+  const [bool, onBoolChange] = useState(false);
+
+  const handleChange = (e) => {
+    // console.log(currDate);
+    onDateChange(e);
+    const diff = Math.floor((currDate.getMonth() -e.getMonth() + 12 * (currDate.getFullYear() - e.getFullYear()))/12);
+    onDiffChange(diff);
+    // console.log(diffValue);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Age Calculator</h1>
+      <div>
+      <DatePicker
+                  onChange={(e)=>handleChange(e)}
+                  value={dateValue}
+                  autoFocus={true}
+                  className="date-picker"
+                  closeCalendar={false}
+               />
+      </div>
+      <br></br>
+      <button onClick={() => {onBoolChange(true)}}>Submit</button>
+      {
+        bool?
+        <h2>You are {diffValue} year(s) old.</h2>: null
+      }
     </div>
   );
 }
